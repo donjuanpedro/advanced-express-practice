a3f92474f8b94c54194f893f7428269dcd175dbc
Walker Lyle <walker@Walkers-Air.attlocal.net>
2018-08-12 21:40:35 -0500

    post routes added

--- server/index.js
+++ server/index.js
@@ -3,8 +3,64 @@ let comments = require("./comments");
 let products = require("./products");
 let vehicles = require("./vehicles");
 let contacts = require("./contacts");
+let bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.get ("/comments", (req,res,) => {
+    return res.json(comments);
+});
+app.get ("/comment/:commentid", (req,res) => {
+    var identity = req.params.commentid;
+    function newFunction(identity) {
+        return res.json(identity);
+    }
+    comments.find(newFunction);
+    return res.json(identity);
+});
+app.post ("/comments", (req,res,) => {
+    let newComment = req.body;
+    comments.push(newComment);
+    res.json(newComment);
+    //req.body.id = 1;
+    // comments.push(req.body)
+    // return res.send(req.body);
+});
+
+
+
+
+app.get ("/products", (req,res) => {
+    return res.json(products);
+});
+app.post ("/products", (req,res,) => {
+    let newProduct = req.body;
+    products.push(newProduct);
+    res.json(newProduct);
+});
+
+app.get ("/vehicles", (req,res) => {
+    return res.json(vehicles);
+});
+app.post ("/vehicles", (req,res,) => {
+    let newVehicle = req.body;
+    vehicles.push(newVehicle);
+    res.json(newVehicle);
+});
+
+
+app.get ("/contacts", (req,res) => {
+    return res.json(contacts);
+});
+app.post ("/contacts", (req,res,) => {
+    let newContact = req.body;
+    contacts.push(newContact);
+    res.json(newContact);
+});
+
+
 
 
 app.listen(3001, (err) => {
@@ -12,31 +68,6 @@ app.listen(3001, (err) => {
         return console.log("Error", err);
     }
     console.log("Web server is on port 3001")
-})
+});
 
 
-app.get ("/products", function(req,res,next){
-    return res.send(products);
-})
-app.get ("/vehicles", function(req,res,next){
-    return res.send(vehicles);
-})
-app.get ("/contacts", function(req,res,next){
-    return res.send(contacts);
-})
-app.get ("/comments", function(req,res,next){
-    return res.send(comments);
-})
-
-app.get ("/products/:id", function(req,res,next){
-    return res.send(products[id]);
-})
-app.get ("/vehicles/:id", function(req,res,next){
-    return res.send(vehicles[id]);
-})
-app.get ("/contacts/:id", function(req,res,next){
-    return res.send(contacts[id]);
-})
-app.get ("/comments/:id", function(req,res,next){
-    return res.send(comments[id]);
-})
