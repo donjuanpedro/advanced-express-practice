df62fa613b892070f62db4ba34d12ef8796c5460
Walker Lyle <walker@walkers-air.lan>
2018-10-12 20:37:19 -0500

    all individual views working

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 
 class Comment extends Component {
     componentDidMount() {
-        let id = this.props.match.params.id;
+        const id = this.props.match.params.id;
         this.props.loadComment(id);
     }
 
     render() {
         return (
             <div>
-                <h3>{this.props.comment.body}</h3>
+                <h3>Comment: {this.props.comment.body}</h3>
             </div>
         )
     }
--- server/modelRoutes/commentRoutes.js
+++ server/modelRoutes/commentRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 let {list,show,create} = require("../controllers/CommentController");
 
 router.get("/comments", list);
-router.get("/comment/:commentid", show);
+router.get("/comment/:id", show);
 router.post("/comments", create);
 // router.put("/comments/:commentid", update);
 // router.delete("/comments/:commentid", remove);
